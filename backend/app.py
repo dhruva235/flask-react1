d119369571fbf518ccf0c8c454043b5df5248c37
Chethan <Chethan27091999@gmail.com>
2022-07-22 19:53:58 +0530

    updated the code to support react frontend

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,33 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
 import pickle
 
 # Create flask app
 flask_app = Flask(__name__)
 model = pickle.load(open("model.pkl", "rb"))
-
+cors = CORS(flask_app)
 @flask_app.route("/")
 def Home():
-    return render_template("index.html")
+    return "Main Route"
 
 @flask_app.route("/predict", methods = ["POST"])
 def predict():
-    float_features = [float(x) for x in request.form.values()]
-    features = [np.array(float_features)]
-    prediction = model.predict(features)
-    return render_template("index.html", prediction_text = "The flower species is {}".format(prediction))
-
+    response={
+        'prediction':None,
+        'status':None
+    }
+    try:
+        requestData=request.json
+        float_features = [float(x) for x in requestData.values()]
+        features = [np.array(float_features)]
+        prediction = model.predict(features)
+        response['prediction']=prediction[0]
+        response['status']=200
+    except:
+        response['status']=200
+    finally:
+        return jsonify(response)
+    
 if __name__ == "__main__":
     flask_app.run(debug=True)
---
+++ widget/src/App.js
@@ -0,0 +1,72 @@
+import './App.css';
+import React,{useState} from 'react';
+import axios from 'axios';
+const App=()=> {
+  const [result,setResult]=useState("");
+  const handleSubmit= async(e)=>{
+    e.preventDefault();
+    const values=e.target.elements;
+    const request={
+      'Sepal_Length':values.Sepal_Length.value,
+      'Sepal_Width':values.Sepal_Width.value,
+      'Petal_Length':values.Petal_Length.value,
+      'Petal_Width':values.Petal_Width.value
+    };
+    try{
+      const response=await axios.post('http://localhost:5000/predict',request);
+      setResult(response.data.prediction);
+    }
+    catch(err){
+      setResult(err);
+    }
+   
+  }
+  return (
+    <div className="container">
+    <div className="d-flex  flex-wrap justify-content-center h-100">
+        <div className="card card-base">
+            <div className="card-header">
+                <div>
+                    <h3>Classifier</h3>
+                </div>
+            </div>
+            <div className="card-body">
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="sepal_length">Sepal Length</label>
+                    <div className="input-group form-group">
+
+                        <input type="number" id="sepal_length" className="form-control"  name="Sepal_Length" placeholder="Length cm" required/>
+                    </div>
+                    <label htmlFor="sepal_width">Sepal Width</label>
+                    <div className="input-group form-group">
+
+                        <input type="number" id="sepal_width" className="form-control"  name="Sepal_Width" placeholder="Width cm" required/>
+                    </div>
+                    <label htmlFor="petal_length">Petal Length</label>
+                    <div className="input-group form-group">
+
+                        <input type="number" id="petal_length" className="form-control"  name="Petal_Length" placeholder="Length cm" required/>
+                    </div>
+                    <label htmlFor="petal_width">Petal Width</label>
+                    <div className="input-group form-group">
+
+                        <input type="number" id="petal_width" className="form-control"  name="Petal_Width" placeholder="Width cm" required/>
+                    </div>
+
+
+                    <div className="form-group text-center">
+                        <button type="submit " value="Predict" className="btn btn-dark button justify-content-center">Submit</button>
+                    </div>
+                </form>
+            </div>
+            <div className="card-body">
+                  <h3>Prediction: <b>{result}</b></h3>
+                </div>
+        </div>
+        
+    </div>
+</div>
+  );
+}
+
+export default App;
